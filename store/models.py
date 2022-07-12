0a217395d9afa4d64b1f475c64f48614a6b36b5b
Sony1974 <sony.mathew.thomas@gmail.com>
2022-07-12 12:44:15 +0300

    second commit - store and carts functionality

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 #from logging.handlers import SYSLOG_UDP_PORT
 #from tkinter import image_names
+from tkinter import CASCADE
 from django.db import models
 from category.models import Category
 from django.urls import reverse
@@ -16,7 +17,7 @@ class Product(models.Model):
     stock               = models.IntegerField()
     is_available        = models.BooleanField(default=True)
     category            = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_data        = models.DateTimeField(auto_now_add=True)
+    created_date        = models.DateTimeField(auto_now_add=True)
     modified_date       = models.DateTimeField(auto_now=True)
 
 
@@ -26,3 +27,29 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = ( 
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product             = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category  = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    #def __unicode__(self):
+        #return self.product
+
+    def __str__(self):
+        return self.variation_value
